9629b72e8a991a93bd93059cb9ddd270296bd31c
L. Yasril Imam <60500658+yasrilimam98@users.noreply.github.com>
2023-11-11 20:28:51 +0700

    custome 404 page

--- src/components/layouts/AppShell/index.tsx
+++ src/components/layouts/AppShell/index.tsx
@@ -5,7 +5,7 @@ type AppShellProps = {
   children: React.ReactNode;
 };
 
-const disabledNavbar = ["/auth/login", "/auth/register"];
+const disabledNavbar = ["/auth/login", "/auth/register", "/404"];
 
 const AppShell = (props: AppShellProps) => {
   const { children } = props;
---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import styles from "@/styles/404.module.scss";
+
+const Custome404 = () => {
+  return (
+    <div className={styles.error}>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+};
+
+export default Custome404;
